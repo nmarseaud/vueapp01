237005252a184e4fdda88ec00dc2a38aea79c946
nmarseaud <nicolas.marseaud@outlook.fr>
2018-02-19 17:31:35 +0100

    Added generator personnalisation + conditions

--- src/components/NpcGenerator.vue
+++ src/components/NpcGenerator.vue
@@ -5,11 +5,46 @@
     <p>
       This generator creates a framework for an original NPC
     </p>
-    <button v-on:click="generateEncounter">Reload</button>
-    <p>
-      A {{ raceSelected }} named {{ nameSelected }} . Is {{ traitSelected }} .
-      {{ quirkSelected }} .
-    </p>
+    <form>
+      <div class="group">
+        <label>Race</label>
+        <select v-on:change="onRaceChange">
+          <option value="">Random</option>
+          <option v-for="(race, index) in races" :key="index">
+            {{ race }}
+          </option>
+        </select>
+        <label>Sexe</label>
+        <select v-on:change="onSexChange" v-model="sexSelected">
+          <option value="">Random</option>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+        </select>
+        <label v-if="hasClass">Power</label>
+        <select v-on:change="onPowerChange" v-if="hasClass" v-model="powerSelected">
+          <option value="">Random</option>
+          <option value="Male">Normal</option>
+          <option value="Female">Heroïc</option>
+          <option value="Female">Legendary</option>
+        </select>
+      </div>
+      <div class="group">
+        <label for="hasClass">Has a class</label>
+        <input type="checkbox" v-model="hasClass" name="hasClass">
+        <label for="hasShop">Has a shop</label>
+        <input type="checkbox" v-model="hasShop" name="hasShop">
+        <label for="hasMagicItem">Has a magic item</label>
+        <input type="checkbox" v-model="hasMagicItem" name="hasMagicItem">
+      </div>
+      <div class="group">
+        <p>A {{ sexSelected }} {{ raceSelected }} named {{ nameSelected }}.</p>
+        <p v-if="hasClass">Is a {{ classSelected }} level {{ levelSelected }}.</p>
+        <p>{{ traitSelected }}.</p>
+        <p>{{ quirkSelected }}.</p>
+        <p v-if="hasShop">{{ quirkShopSelected }}.</p>
+      </div>
+      <button v-on:click="generateEncounter" type="button">Reload</button>
+    </form>
   </div>
 </template>
 
@@ -19,6 +54,7 @@ import physics from '@/assets/json/physics.json'
 import quirks from '@/assets/json/quirks.json'
 import races from '@/assets/json/races.json'
 import traits from '@/assets/json/traits.json'
+
 export default {
   name: 'NpcGenerator',
   data: function () {
@@ -26,35 +62,62 @@ export default {
       names: names,
       physics: physics,
       quirks: quirks,
+      raceRandom: true,
       races: races,
       traits: traits,
       nameSelected: '',
       physicSelected: '',
       quirkSelected: '',
+      quirkShopSelected: '',
       raceSelected: '',
-      traitSelected: ''
+      traitSelected: '',
+      sexSelected: '',
+      powerSelected: '',
+      selectedRace: null,
+      hasClass: false,
+      hasShop: false,
+      hasMagicItem: false
     }
   },
   created () {
-    var randomRaces = Math.floor(Math.random() * Math.floor(races.length))
-    this.raceSelected = races[randomRaces]
-
-    var randomQuirks = Math.floor(Math.random() * Math.floor(quirks[0].Vornheim.length))
-    this.quirkSelected = quirks[0].Vornheim[randomQuirks]
-
-    var randomTraits = Math.floor(Math.random() * Math.floor(traits[0].Vornheim.length))
-    this.traitSelected = traits[0].Vornheim[randomTraits]
+    this.generateEncounter()
   },
   methods: {
     generateEncounter: function (event) {
-      var randomRaces = Math.floor(Math.random() * Math.floor(races.length))
-      this.raceSelected = races[randomRaces]
+      if (this.raceSelected === '' || this.raceRandom) {
+        console.log('pouet')
+        var randomRaces = Math.floor(Math.random() * Math.floor(races.length - 1))
+        this.raceSelected = races[randomRaces]
+      }
+      if (this.sexSelected === '') {
+        var randomSex = Math.floor(Math.random() * Math.floor(1))
+        var sex = ['Male', 'Female']
+        this.sexSelected = sex[randomSex]
+      }
 
-      var randomQuirks = Math.floor(Math.random() * Math.floor(quirks[0].Vornheim.length))
+      var nameList = names.find(o => o.Race === this.raceSelected)
+      var randomName = Math.floor(Math.random() * Math.floor(nameList[this.sexSelected].length - 1))
+      this.nameSelected = nameList[this.sexSelected][randomName]
+
+      var randomQuirks = Math.floor(Math.random() * Math.floor(quirks[0].Vornheim.length - 1))
       this.quirkSelected = quirks[0].Vornheim[randomQuirks]
+      console.log(this.hasShop)
+      if (this.hasShop) {
+        var randomShopQuirks = Math.floor(Math.random() * Math.floor(quirks[1].Vornheim_shop.length - 1))
+        this.quirkShopSelected = quirks[1].Vornheim_shop[randomShopQuirks]
+      }
 
-      var randomTraits = Math.floor(Math.random() * Math.floor(traits[0].Vornheim.length))
+      var randomTraits = Math.floor(Math.random() * Math.floor(traits[0].Vornheim.length - 1))
       this.traitSelected = traits[0].Vornheim[randomTraits]
+    },
+    onRaceChange: function (event) {
+      this.raceSelected = event.target.value
+      if (event.target.value !== '') this.raceRandom = false
+      this.generateEncounter()
+    },
+    onSexChange: function (event) {
+      this.sexSelected = event.target.value
+      this.generateEncounter()
     }
   }
 }
@@ -75,5 +138,12 @@ li {
 }
 a {
   color: #42b983;
+}
+.v-select {
+  display: inline-block;
+  width: 400px;
+}
+.group {
+  padding-top: 10px;
 }
 </style>
